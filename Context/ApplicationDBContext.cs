3bfdb671db2259829760a3fe8f21e50e741fe3d2
adhdhdh <135920314+AdyGomezMtz@users.noreply.github.com>
2023-07-06 15:52:39 -0500

    Agregar archivos de proyecto.

---
+++ Context/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto23CV.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto23CV.Context
+{
+    public class ApplicationDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server= localhost; database=ProyectoDb23CV; user=root; password=");
+        }
+
+        public DbSet<Rol>Roles { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
